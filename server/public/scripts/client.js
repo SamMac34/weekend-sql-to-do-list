1385a8bf51db8607cb8980ac753b7063545a4f2d
Sam McCauley <sammccauley34@gmail.com>
2023-06-24 13:54:47 -0500

    Added base code to client/server/router files to bring in and import/export express,pg, routers, etc.  Added comment lines for necessary functions in each file

---
+++ server/public/scripts/client.js
@@ -0,0 +1,21 @@
+$(document).ready();
+console.log('JS Ready!');
+
+onReady( {
+    console.log('JQ Ready');
+
+
+
+});
+
+// handleSubmit
+
+// Add a task
+
+// Refresh taskList
+
+// Update a task to completed
+
+// Delete a task
+
+// Render taskList
---
+++ server/routes/task.router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const pool = require('../modules/pool');
+
+
+// GET all tasks
+
+// POST new task
+
+// PUT update task to completed
+
+// DELETE a task
+
+
+module.exports = router;
