b14f2dfb0b9369fe69e91f43c607dc695ba272f4
Sam McCauley <sammccauley34@gmail.com>
2023-06-24 13:37:53 -0500

    setup server.js to bring in express and router

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const taskRouter = require('./routes/task.router.js');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('server/public'));
+app.use('/tasks', taskRouter);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+});
